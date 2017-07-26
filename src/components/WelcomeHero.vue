7086b4bec2f62ccfa3ecaee359ef37b1f6b8f1e3
Pedro Araújo <sight.pedro@gmail.com>
2017-07-26 10:03:41 +0100

    update stylings & feature base

--- src/components/WelcomeHero.vue
+++ src/components/WelcomeHero.vue
@@ -2,22 +2,39 @@
   <section class="Hero">
     <div class="Hero-radialPulse" :style="radialAnimation"></div>
 
-    <div class="Hero-wrapper u-marginTop--medium">
-      <div class="Hero-welcomeMsg">
-        Upload music file to see tempo stats
+    <div class="Hero-wrapper u-marginTop--small">
+      <div class="Hero-textTitle">
+        Get Rhythm stats for your music
       </div>
 
-      <div class="u-marginTop--medium">
-        <button class="Button">
-          <icon class="u-marginRight--xSmall" name="upload"></icon>
-          <span>Upload</span>
-        </button>
+      <div class="Hero-uploadZone u-marginTop--medium">
+        <div class="Hero-uploadForm">
+          <button class="Button">
+            <icon name="upload"></icon>
+            <span class="u-marginLeft--xSmall">Upload</span>
+          </button>
+
+          <aside>
+            <div class="Hero-extensionTitle u-marginTop--xSmall">
+              Supported extensions:
+            </div>
+            <small class="Hero-extensionList">MP3, WAV, OGG, ACC</small>
+          </aside>
+        </div>
+
+        <div class="Hero-textTitle">
+          Or
+        </div>
+
+        <div class="Hero-recordForm u-marginLeft--medium">
+          <button class="Button">
+            <icon name="microphone"></icon>
+            <span class="u-marginLeft--xSmall">Record</span>
+          </button>
+        </div>
+
       </div>
 
-      <div class="Hero-extensionTitle u-marginTop--xSmall">
-        Supported extensions:
-      </div>
-      <small class="Hero-extensionList">MP3, WAV, OGG, ACC</small>
 
     </div>
 
